5478398bb71d66e2a1c6d1c9189d5ee3bf16024a
node <node1729@gmail.com>
2020-04-29 12:10:59 -0500

    dev files

--- discord/test2.py
+++ discord/test2.py
@@ -6,11 +6,9 @@ class OutLaugh():
     def __init__(self, max_players=8):
         self.max_players = max_players
         self.players = []
-        self.f = open("questions.txt")
+        self.f = open("questions_all.txt")
         self.questions = self.f.readlines()
-        self.user = user
         self.pairs = []
-        self.deny_players = []
     
     # async def on_message(self, message):
     #     if message.author != self.user:
@@ -29,41 +27,49 @@ class OutLaugh():
     # returns true if able to add a new player
     def add_player(self, player, max_players=8):
         if len(self.players) < max_players:
-            if player not in self.players
+            if player not in self.players:
                 self.players.append(player)
                 return True
         return False
     
     # TODO: implement main game logic to start the game
-    # TODO: implement swap_player() to ensure that no player is left without a pair
     def start_game(self):
-        players1 = self.players
-        players2 = copy.deepcopy(self.players) # create new arrays for both player sets
-        pairs = []
-        for p1 in players1:
-            p2 = random.choice(players2)
-            pair = [p1, p2]
-            for p in self.pairs:
-                while p[0] == pair[1] and p[1] == pair[0]:
-                    p2 = random.choice(players2)
-                    pair = [p1, p2]
-            players2.remove(p2)
-            self.pairs.append(pair)
-        pairs_disp = []
+        while not self.set_pairs():
+            pass
+        pairs_disp = []            
         for item in self.pairs:
             player_out = []
             for player in item:
                 player_out.append(player.id)
             pairs_disp.append(player_out)
         print(pairs_disp)
-        for pair in self.pairs:
-            Question.ask_question()
+        # for pair in self.pairs:
+            # Question.ask_question()
 
     def get_question(self):
         question = random.choice(self.questions)
         self.questions.remove(question)
         return question
-            
+    
+    # Sets the pairs for the game. This is meant to be called in a while loop, as seen in start_game()
+    # Returns True upon a successful configuration
+    # Credit to AshEevee_ for coming up with this algorithm
+    def set_pairs(self):
+        players1 = self.players
+        players2 = self.players.copy() # create new arrays for both player sets
+        pairs = self.pairs = []
+        for p1 in players1:
+            p2 = random.choice(players2)
+            if p1.id == p2.id:
+                return False
+            pair = [p1, p2]
+            for p in self.pairs:
+                if p[0].id == pair[1].id and p[1].id == pair[0].id:
+                    return False
+                pair = [p1, p2]
+            players2.remove(p2)
+            self.pairs.append(pair)
+        return True
 
 class Question(OutLaugh):
     def __init__(self, question, players):
@@ -90,8 +96,9 @@ class GamePlayer:
     def add_points(self, num):
         self.points += 500 * num
 
+# while True:
 while True:
     test = OutLaugh()
-    for i in range(20):
+    for i in range(3):
         test.add_player(GamePlayer(i, i))
     test.start_game()
